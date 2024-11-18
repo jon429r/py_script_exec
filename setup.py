5394b02e3c3f467e2a9af115b275c787e58c77f9
jon429r <jonathanday088@gmail.com>
2024-11-18 14:49:21 -0500

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_script_exec",
-    version="1.1.0",
+    version="1.2.0",
     description="A cross-platform Python script runner.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
